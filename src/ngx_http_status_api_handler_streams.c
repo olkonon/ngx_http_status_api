8f5a19ef78e4c6a6dd81cc99ed04d469aa75c60f
Kononenko Oleg <o.kononenko@qiwi.com>
2025-04-21 12:55:31 +0300

    Fix build

--- src/ngx_http_status_api_handler_streams.c
+++ src/ngx_http_status_api_handler_streams.c
@@ -1,10 +1,15 @@
 //
 // Created by o.kononenko on 21.04.2025.
 //
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
 
 #include "ngx_http_status_api_handler_streams.h"
-#include "ngx_http_stream_server_traffic_status_display.h"
 
+#ifdef NGX_STREAM_STS_STATUS
+#include "ngx_http_stream_server_traffic_status_display.h"
+#endif
 
 ngx_int_t ngx_http_status_api_handler_streams_handler(ngx_http_request_t *r) {
 #ifdef NGX_STREAM_STS_STATUS
